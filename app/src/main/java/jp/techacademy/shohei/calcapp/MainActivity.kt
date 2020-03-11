2018729b5175577f946996348b0ae97b5acd8a7f
Mackey912 <shohei10912@gamil.com>
2020-03-11 14:19:33 +0900

    ・数値が入っていない時の割り算をできないように変更 ・0割禁止機能追加 ・ドット"."入力禁止機能追加

--- app/src/main/java/jp/techacademy/shohei/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/shohei/calcapp/MainActivity.kt
@@ -22,11 +22,14 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
     }
 
     override fun onClick(v:View){
-        if(editText1.text.toString().length!=0||editText2.text.toString().length!=0) {
+        if(editText1.text.toString().length!=0&&editText2.text.toString().length!=0
+            &&editText1.text.toString()!="."&&editText2.text.toString()!=".") {
+
             val intent = Intent(this, CalcOut::class.java)
             var val1: Float = editText1.text.toString().toFloat()
             var val2: Float = editText2.text.toString().toFloat()
             var send: String
+
             if (v.id == R.id.button_plus) {
                 send = (val1 + val2).toString()
             } else if (v.id == R.id.button_minus) {
@@ -34,11 +37,20 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
             } else if (v.id == R.id.button_time) {
                 send = (val1 * val2).toString()
             } else {
-                send = (val1 / val2).toString()
+                if(val2!=0F) {
+                    send = (val1 / val2).toString()
+                }else{
+                    Snackbar.make(v, "0での割り算はできません", Snackbar.LENGTH_LONG)
+                        .setAction("Action",null).show()
+                    return
+                }
             }
+
             intent.putExtra("VALUE1", send)
             startActivity(intent)
+
         }else{
+
             Snackbar.make(v, "何か数値を入力してください", Snackbar.LENGTH_LONG)
                 .setAction("Action",null).show()
         }
